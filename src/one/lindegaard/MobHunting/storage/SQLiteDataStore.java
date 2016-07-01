25b732c9360e123c676f68e9be5b5c99b5c4161f
Rocologo <rocologo@rocologo.tk>
2016-07-01 15:11:54 +0200

    V3.2.7-SNAPSHOT

--- src/one/lindegaard/MobHunting/storage/SQLiteDataStore.java
+++ src/one/lindegaard/MobHunting/storage/SQLiteDataStore.java
@@ -145,10 +145,10 @@ public class SQLiteDataStore extends DatabaseDataStore {
 							+ (id != null ? " where mh_Players.NAME!='' and ID=" + id : "") + " order by "
 							+ type.getDBColumn() + " desc limit " + count);
 			while (results.next()) {
-				OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(results.getString(3));
+				OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(results.getString(2)));
 				if (offlinePlayer == null)
 					MobHunting.debug("getOfflinePlayer(%s) was not in cache.", results.getString(3));
-				if (offlinePlayer != null)
+				else
 					list.add(new StatStore(type, offlinePlayer, results.getInt(1)));
 			}
 			results.close();
