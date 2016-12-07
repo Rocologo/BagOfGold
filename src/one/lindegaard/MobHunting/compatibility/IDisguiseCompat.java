57c303330371dc76a2d5146045b5d60167bd86e0
Rocologo <rocologo@rocologo.tk>
2016-12-07 23:51:18 +0100

    V4.2.0 Fix iDisguise support on Minecraft 1.9.4
    * Added missing Villagers: Librarian, Priest, Butcher, Blacksmith,
    Farmer. 
    * Attacking the MasterMobHunter with Sentinel Trait makes him attack the
    player.
    * Fixed LibsDisguises compatibility on Minecraft 1.9.4
    * Fixed iDisguises compatibility on Minecraft 1.9.4

--- src/one/lindegaard/MobHunting/compatibility/IDisguiseCompat.java
+++ src/one/lindegaard/MobHunting/compatibility/IDisguiseCompat.java
@@ -6,8 +6,9 @@ import java.util.Set;
 
 import de.robingrether.idisguise.api.*;
 import de.robingrether.idisguise.disguise.*;
-//import de.robingrether.idisguise.iDisguise;
+import de.robingrether.idisguise.disguise.DisguiseType;
 import one.lindegaard.MobHunting.MobHunting;
+import one.lindegaard.MobHunting.util.Misc;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -24,6 +25,83 @@ public class IDisguiseCompat implements Listener {
 	private static Plugin mPlugin;
 	private static DisguiseAPI api;
 	private static boolean supported = false;
+
+	private static DisguiseType[] aggresiveList = new DisguiseType[30];
+	private static DisguiseType[] passiveList = new DisguiseType[20];
+	private static DisguiseType[] otherList = new DisguiseType[10];
+
+	static {
+		int n = 0;
+		if (Misc.isMC111OrNewer()) {
+			aggresiveList[n++] = DisguiseType.VEX;
+			aggresiveList[n++] = DisguiseType.EVOKER;
+			aggresiveList[n++] = DisguiseType.VINDICATOR;
+		}
+		if (Misc.isMC111OrNewer()) {
+			aggresiveList[n++] = DisguiseType.HUSK;
+			aggresiveList[n++] = DisguiseType.STRAY;
+			aggresiveList[n++] = DisguiseType.POLAR_BEAR;
+		}
+		if (Misc.isMC19OrNewer()) {
+			aggresiveList[n++] = DisguiseType.SHULKER;
+		}
+		aggresiveList[n++] = DisguiseType.ZOMBIE;
+		aggresiveList[n++] = DisguiseType.BLAZE;
+		aggresiveList[n++] = DisguiseType.CAVE_SPIDER;
+		aggresiveList[n++] = DisguiseType.CREEPER;
+		aggresiveList[n++] = DisguiseType.ELDER_GUARDIAN;
+		aggresiveList[n++] = DisguiseType.ENDER_DRAGON;
+		aggresiveList[n++] = DisguiseType.ENDERMAN;
+		aggresiveList[n++] = DisguiseType.ENDERMITE;
+		aggresiveList[n++] = DisguiseType.GHAST;
+		aggresiveList[n++] = DisguiseType.GIANT;
+		aggresiveList[n++] = DisguiseType.GUARDIAN;
+		aggresiveList[n++] = DisguiseType.PIG_ZOMBIE;
+		aggresiveList[n++] = DisguiseType.SKELETON;
+		aggresiveList[n++] = DisguiseType.SLIME;
+		aggresiveList[n++] = DisguiseType.SPIDER;
+		aggresiveList[n++] = DisguiseType.WITCH;
+		aggresiveList[n++] = DisguiseType.WITHER;
+		aggresiveList[n++] = DisguiseType.WITHER_SKELETON;
+		aggresiveList[n++] = DisguiseType.ZOMBIE;
+		aggresiveList[n++] = DisguiseType.ZOMBIE_VILLAGER;
+		aggresiveList[n++] = DisguiseType.SHULKER;
+	}
+	private static final Set<DisguiseType> aggresiveMobs = new HashSet<DisguiseType>(Arrays.asList(aggresiveList));
+
+	static {
+		int n2 = 0;
+		passiveList[n2++] = DisguiseType.BAT;
+		passiveList[n2++] = DisguiseType.CHICKEN;
+		passiveList[n2++] = DisguiseType.COW;
+		passiveList[n2++] = DisguiseType.DONKEY;
+		passiveList[n2++] = DisguiseType.HORSE;
+		passiveList[n2++] = DisguiseType.IRON_GOLEM;
+		passiveList[n2++] = DisguiseType.MAGMA_CUBE;
+		passiveList[n2++] = DisguiseType.MULE;
+		passiveList[n2++] = DisguiseType.MUSHROOM_COW;
+		passiveList[n2++] = DisguiseType.OCELOT;
+		passiveList[n2++] = DisguiseType.PIG;
+		passiveList[n2++] = DisguiseType.RABBIT;
+		passiveList[n2++] = DisguiseType.SHEEP;
+		passiveList[n2++] = DisguiseType.SILVERFISH;
+		passiveList[n2++] = DisguiseType.SNOWMAN;
+		passiveList[n2++] = DisguiseType.SQUID;
+	}
+	private static final Set<DisguiseType> passiveMobs = new HashSet<DisguiseType>(Arrays.asList(passiveList));
+
+	static {
+		int n3 = 0;
+		otherList[n3++] = DisguiseType.ARMOR_STAND;
+		otherList[n3++] = DisguiseType.BOAT;
+		otherList[n3++] = DisguiseType.ENDER_CRYSTAL;
+		otherList[n3++] = DisguiseType.FALLING_BLOCK;
+		otherList[n3++] = DisguiseType.MINECART;
+		otherList[n3++] = DisguiseType.PLAYER;
+		otherList[n3++] = DisguiseType.VILLAGER;
+		otherList[n3++] = DisguiseType.WOLF;
+	}
+	private static final Set<DisguiseType> otherDisguiseTypes = new HashSet<DisguiseType>(Arrays.asList(otherList));
 
 	public IDisguiseCompat() {
 		if (MobHunting.getConfigManager().disableIntegrationIDisguise) {
@@ -75,27 +153,25 @@ public class IDisguiseCompat implements Listener {
 			api.undisguise((Player) entity);
 	}
 
-	private static final DisguiseType aggresiveList[] = { DisguiseType.ZOMBIE, DisguiseType.BLAZE,
-			DisguiseType.CAVE_SPIDER, DisguiseType.CREEPER, DisguiseType.ENDER_DRAGON, DisguiseType.ENDERMAN,
-			DisguiseType.ENDERMITE, DisguiseType.GHAST, DisguiseType.GIANT, DisguiseType.GUARDIAN,
-			DisguiseType.PIG_ZOMBIE, DisguiseType.SKELETON, DisguiseType.SLIME, DisguiseType.SPIDER, DisguiseType.WITCH,
-			DisguiseType.WITHER, DisguiseType.ZOMBIE };
-	// TODO: SHULKER not supported by IDisguise yet???
-
-	public static final Set<DisguiseType> aggresiveMobs = new HashSet<DisguiseType>(Arrays.asList(aggresiveList));
-
-	private static final DisguiseType passiveList[] = { DisguiseType.BAT, DisguiseType.CHICKEN, DisguiseType.COW,
-			DisguiseType.HORSE, DisguiseType.IRON_GOLEM, DisguiseType.MAGMA_CUBE, DisguiseType.MUSHROOM_COW,
-			DisguiseType.OCELOT, DisguiseType.PIG, DisguiseType.RABBIT, DisguiseType.SHEEP, DisguiseType.SILVERFISH,
-			DisguiseType.SNOWMAN, DisguiseType.SQUID, };
-	public static final Set<DisguiseType> passiveMobs = new HashSet<DisguiseType>(Arrays.asList(passiveList));
-
-	private static final DisguiseType otherList[] = { DisguiseType.PLAYER, DisguiseType.VILLAGER, DisguiseType.WOLF };
-	public static final Set<DisguiseType> otherDisguiseTypes = new HashSet<DisguiseType>(Arrays.asList(otherList));
-
 	public static boolean isAggresiveDisguise(Entity entity) {
 		Disguise d = getDisguise(entity);
 		if (aggresiveMobs.contains(d.getType()))
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean isPassiveDisguise(Entity entity) {
+		Disguise d = getDisguise(entity);
+		if (passiveMobs.contains(d.getType()))
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean isOtherDisguise(Entity entity) {
+		Disguise d = getDisguise(entity);
+		if (otherDisguiseTypes.contains(d.getType()))
 			return true;
 		else
 			return false;
