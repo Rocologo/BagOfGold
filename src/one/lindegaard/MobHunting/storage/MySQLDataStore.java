d5f98c09554fc2f61684a5128fb8794f3902fea1
Rocologo <rocologo@rocologo.tk>
2016-12-18 14:05:54 +0100

    V4.2.5 Bug fixing
    * Fixed missing lines in mythicMobs-reward.yml file.
    (https://www.spigotmc.org/threads/mobhunting.46937/page-74#post-2083797
    )
    * Fix for MythicMobs which are deleted from the MythicMobs configuration
    files, but still exists in MobHunting "mythicmobs-rewards.yml" file.
    * Fix for Citizens which are deleted from the saves.yml configuration
    file, but still exists in MobHunting "citizens-rewards.yml" file.
    * Fix for CustomMobs which are deleted from the CustomMobs configuration
    file, but still exists in MobHunting "custommobs-rewards.yml" file.
    * Fixed Minimums reward onAssisted kills
    * Added a check for MythicMobs version. MobHunting only support
    MythicMobs V2.5.1 and newer.
    * Added missing text in lang files:
    mobhunting.commands.database.description=Fixing problems in the
    database.
    * Disabled Infinity Speed / Resistance bonus effect on creepers. 
    * Changed mh_Bounties to allow null in BOUNTYOWNER_ID, used for random
    bounties.

--- src/one/lindegaard/MobHunting/storage/MySQLDataStore.java
+++ src/one/lindegaard/MobHunting/storage/MySQLDataStore.java
@@ -1003,14 +1003,16 @@ public class MySQLDataStore extends DatabaseDataStore {
 			try {
 				ResultSet rs = create
 						.executeQuery("SELECT PLAYER_ID from mh_Players WHERE NAME='RandomBounty' LIMIT 0");
+				if (!rs.next()) {
+					System.out.println("[MobHunting] Adding RandomBounty (player_id) to MobHunting Database.");
+					create.executeUpdate(
+							"insert into mh_Players (UUID,NAME,PLAYER_ID,LEARNING_MODE,MUTE_MODE) values (null,'RandomBounty',0,0,0)");
+					create.executeUpdate("update mh_Players set Player_id=0 where name='RandomBounty'");
+				}
+				;
 				rs.close();
 			} catch (SQLException e) {
-				System.out.println("[MobHunting] Adding RandomBounty (player_id) to MobHunting Database.");
-				create.executeUpdate(
-						"insert into mh_Players (UUID,NAME,PLAYER_ID,LEARNING_MODE,MUTE_MODE) values (null,'RandomBounty',0,0,0)");
-				create.executeUpdate("update mh_Players set Player_id=0 where name='RandomBounty'");
 			}
-
 		}
 
 		// Setup Database triggers
