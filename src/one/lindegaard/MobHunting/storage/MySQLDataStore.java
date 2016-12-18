699279eb310f9a092a3fe797da11d23888b13169
Rocologo <rocologo@rocologo.tk>
2016-12-18 11:59:11 +0100

    V4.2.5 Bug fixing
    * Fixed missing lines in mythicMobs-reward.yml file.
    (https://www.spigotmc.org/threads/mobhunting.46937/page-74#post-2083797
    )
    * Fix for MythicMobs which are deleted from the MythicMobs configuration
    files, but still exists in MobHunting "mythicmobs-rewards.yml" file.
    * Fix for Citizens which are deleted from the saves.yml configuration
    file, but still exists in MobHunting "citizens-rewards.yml" file.
    * Fix for CustomMobs which are deleted from the CustomMobs configuration
    file, but still exists in MobHunting "custommobs-rewards.yml" file.
    * Fixed Minimums reward onAssisted kills
    * Added a check for MythicMobs version. MobHunting only support
    MythicMobs V2.5.1 and newer.
    * Added missing text in lang files:
    mobhunting.commands.database.description=Fixing problems in the
    database.
    * Disabled Infinity Speed / Resistance bonus effect on creepers. 
    * Changed mh_Bounties to allow null in BOUNTYOWNER_ID, used for random
    bounties.

--- src/one/lindegaard/MobHunting/storage/MySQLDataStore.java
+++ src/one/lindegaard/MobHunting/storage/MySQLDataStore.java
@@ -998,19 +998,22 @@ public class MySQLDataStore extends DatabaseDataStore {
 					+ "CONSTRAINT mh_Bounties_Player_Id_1 FOREIGN KEY(BOUNTYOWNER_ID) REFERENCES mh_Players(PLAYER_ID) ON DELETE CASCADE, "
 					+ "CONSTRAINT mh_Bounties_Player_Id_2 FOREIGN KEY(WANTEDPLAYER_ID) REFERENCES mh_Players(PLAYER_ID) ON DELETE CASCADE"
 					+ ")");
-			
-			
-			//added because BOUNTYOWNER_ID is null for Random bounties.
+
+			// added because BOUNTYOWNER_ID is null for Random bounties.
 			Statement statement = connection.createStatement();
 			try {
 				ResultSet rs = statement.executeQuery("SELECT PLAYER_ID from mh_Players WHERE NAME='RandomBounty'");
+				if (rs.isClosed()) {
+					System.out.println("[MobHunting] Adding RandomBounty (player_id) to MobHunting Database.");
+					statement.executeUpdate(
+							"insert into mh_Players (UUID,NAME,PLAYER_ID,LEARNING_MODE,MUTE_MODE) values (null,'RandomBounty',0,0,0)");
+					statement.executeUpdate("update mh_Players set Player_id=0 where name='RandomBounty'");
+				}
 				rs.close();
 			} catch (SQLException e) {
-				System.out.println("[MobHunting] Adding RandomBounty (player_id) to MobHunting Database.");
-				statement.executeUpdate("insert into mh_Players (NAME,PLAYER_ID,LEARNING_MODE,MUTE_MODE) values ('RandomBounty',0,0,0)");
-				statement.executeUpdate("update mh_Players set Player_id=0 where name='RandomBounty'");
+				e.printStackTrace();
 			}
-			
+
 		}
 
 		// Setup Database triggers
