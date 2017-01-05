033d5b6b8ac0e2aeae6aa6934e85e1035fe2017f
Rocologo <rocologo@rocologo.tk>
2017-01-05 18:36:39 +0100

    V4.2.8 New feature: Support for Conquestia Mobs added + Bug fixing
    * Added support for Conquestia Mobs
    (https://www.spigotmc.org/resources/conquesita-mobs.21307/)
    * Removed some SPAM from the console.
    (https://www.spigotmc.org/threads/mobhunting.46937/page-78#post-2120503)
    * Fixed NPE error i rare cases in the Citizens integration.
    (https://www.spigotmc.org/threads/mobhunting.46937/page-79#post-2134012)
    * New feature - now handle Custom spawned mobs with SpawnReason.CUSTOM
    * Removed SQL lock when player joins the server. Thanks to Maxetto and
    Games647. (https://github.com/Rocologo/MobHunting/issues/108)

--- src/one/lindegaard/MobHunting/MobHuntingManager.java
+++ src/one/lindegaard/MobHunting/MobHuntingManager.java
@@ -1360,7 +1360,7 @@ public class MobHuntingManager implements Listener {
 		if (event.getSpawnReason() == SpawnReason.CUSTOM) {
 			if (!MobHunting.getConfigManager().allowCustomMobsSpawners) {
 				Messages.debug("%s was spawned with SpawnReason.CUSTOM", event.getEntityType());
-				event.getEntity().setMetadata("MH:blocked", new FixedMetadataValue(MobHunting.getInstance(), true));
+				//event.getEntity().setMetadata("MH:blocked", new FixedMetadataValue(MobHunting.getInstance(), true));
 			}
 		} else if (event.getSpawnReason() == SpawnReason.SPAWNER || event.getSpawnReason() == SpawnReason.SPAWNER_EGG) {
 			if (!MobHunting.getConfigManager().allowMobSpawners)
