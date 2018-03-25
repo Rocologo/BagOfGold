89d5c835eaa420996015c56868455a22ff41adc1
Rocologo <rocologo@hotmail.com>
2018-03-25 18:19:47 +0200

    V1.0.8

--- src/one/lindegaard/BagOfGold/storage/DataStoreManager.java
+++ src/one/lindegaard/BagOfGold/storage/DataStoreManager.java
@@ -82,7 +82,7 @@ public class DataStoreManager {
 	 * @param offlinePlayer
 	 * @param playerSetting
 	 */
-	public void updatePlayerSettings2(OfflinePlayer offlinePlayer, PlayerSettings ps) {
+	public void updatePlayerSettings(OfflinePlayer offlinePlayer, PlayerSettings ps) {
 		synchronized (mWaiting) {
 			mWaiting.add(new PlayerSettings(offlinePlayer, ps));
 		}
