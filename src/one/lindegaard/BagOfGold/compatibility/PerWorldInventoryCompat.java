2ddbbc06cba5be9cb9e69f4f7e10087ca6dbd04c
Rocologo <rocologo@hotmail.com>
2018-08-09 20:52:17 +0200

    Remove onLoadInventoryCompleted.

--- src/one/lindegaard/BagOfGold/compatibility/PerWorldInventoryCompat.java
+++ src/one/lindegaard/BagOfGold/compatibility/PerWorldInventoryCompat.java
@@ -42,15 +42,15 @@ public class PerWorldInventoryCompat implements Listener {
 			if (sync_economy)
 				pwi_sync_economy_warning();
 
-			if (mPlugin.getDescription().getVersion().compareTo("2.1.0") >= 0)
-				Bukkit.getPluginManager().registerEvents(new Listener() {
-					@EventHandler(priority = EventPriority.HIGHEST)
-					public void onInventoryChangeCompleted(InventoryLoadCompleteEvent event) {
-						plugin.getMessages().debug("onInventoryLoadCompleted");
-						plugin.getEconomyManager().adjustAmountInInventoryToBalance(event.getPlayer());
-					}
-				}, plugin);
-			else
+			//if (mPlugin.getDescription().getVersion().compareTo("2.1.0") >= 0)
+			//	Bukkit.getPluginManager().registerEvents(new Listener() {
+			//		@EventHandler(priority = EventPriority.HIGHEST)
+			//		public void onInventoryChangeCompleted(InventoryLoadCompleteEvent event) {
+			//			plugin.getMessages().debug("onInventoryLoadCompleted");
+			//			plugin.getEconomyManager().adjustAmountInInventoryToBalance(event.getPlayer());
+			//		}
+			//	}, plugin);
+			//else
 				Bukkit.getPluginManager().registerEvents(new Listener() {
 					@EventHandler(priority = EventPriority.HIGHEST)
 					public void onInventoryLoad(InventoryLoadEvent event) {
