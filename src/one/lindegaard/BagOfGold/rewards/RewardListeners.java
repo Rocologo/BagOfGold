115d01290c9eecd0b5d949646de54bd06b61dd3f
Rocologo <rocologo@hotmail.com>
2018-09-01 11:01:18 +0200

    Fixed ticket 33 ItemFrame causes doubling money
    https://github.com/Rocologo/BagOfGold/issues/33

--- src/one/lindegaard/BagOfGold/rewards/RewardListeners.java
+++ src/one/lindegaard/BagOfGold/rewards/RewardListeners.java
@@ -1,16 +1,26 @@
 package one.lindegaard.BagOfGold.rewards;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import one.lindegaard.BagOfGold.BagOfGold;
 import one.lindegaard.BagOfGold.PlayerBalance;
+import one.lindegaard.BagOfGold.Reward;
 import one.lindegaard.BagOfGold.compatibility.PerWorldInventoryCompat;
 import one.lindegaard.BagOfGold.util.Misc;
 
@@ -81,5 +91,28 @@ public class RewardListeners implements Listener {
 		plugin.getMessages().debug("RewardListernes: PlayerChangedWorld %s (from %s to %s) new balance is %s",
 				player.getName(), event.getFrom(), event.getPlayer().getWorld(),
 				plugin.getEconomyManager().getBalance(player));
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event) {
+		
+		if (event.isCancelled())
+			return;
+
+		if (event.getRightClicked().getLocation()==null)
+			return;
+		
+		Player player = event.getPlayer();
+
+		if (event.getRightClicked().getType()==EntityType.ITEM_FRAME && Reward.isReward(player.getInventory().getItemInMainHand())){
+			Reward reward=Reward.getReward(player.getInventory().getItemInMainHand());
+			if (reward.getMoney()!=0){
+				plugin.getMessages().debug("%s placed a BagOfGod in an ItemFrame", player.getName());
+				plugin.getEconomyManager().removeMoneyFromBalance(player, reward.getMoney());
+				plugin.getMessages().playerActionBarMessageQueue(player,
+						ChatColor.valueOf(plugin.getConfigManager().dropMoneyOnGroundTextColor)
+								+ reward.getDisplayname()+plugin.getMessages().getString("bagofgold.moneydrop","money",Misc.round(reward.getMoney())));
+			}
+		}
 	}
 }
