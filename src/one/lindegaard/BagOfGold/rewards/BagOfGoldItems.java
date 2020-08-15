c2843ec77064a5ee01116fd3b9c953776d7d6fd4
Rocologo <rocologo@hotmail.com>
2020-08-15 22:54:25 +0200

    Fixet ticket 670

--- src/one/lindegaard/BagOfGold/rewards/BagOfGoldItems.java
+++ src/one/lindegaard/BagOfGold/rewards/BagOfGoldItems.java
@@ -936,36 +936,29 @@ public class BagOfGoldItems implements Listener {
 					}
 					break;
 				case MOVE_TO_OTHER_INVENTORY:
-					if ((Reward.isReward(isCurrentSlot) || Reward.isReward(isCursor))
-							&& inventory.getType() != InventoryType.ANVIL
-							&& inventory.getType() != InventoryType.ENCHANTING
-							&& inventory.getType() != InventoryType.CRAFTING) {
+					if ((Reward.isReward(isCurrentSlot) || Reward.isReward(isCursor))) {
 						Reward reward = Reward.isReward(isCurrentSlot) ? Reward.getReward(isCurrentSlot)
 								: Reward.getReward(isCursor);
-						int amount = Reward.isReward(isCurrentSlot) ? isCurrentSlot.getAmount() : isCursor.getAmount();
 						if (reward.isMoney()) {
-							// if (slotType != SlotType.CONTAINER ) { //&& slotType != SlotType.QUICKBAR) {
-							if (clickedInventory.getType() == InventoryType.PLAYER) {
-								plugin.getMessages().debug("%s moved %s %s out of the Player Inventory",
-										player.getName(), reward.getMoney() * amount, reward.getDisplayName());
-								plugin.getRewardManager().removeMoneyFromPlayerBalance(player,
-										reward.getMoney() * amount);
-							} else { // CHEST, DISPENSER, DROPPER, ......
-								plugin.getMessages().debug("%s moved %s %s into the Player Inventory", player.getName(),
-										reward.getMoney() * amount, reward.getDisplayName());
-								plugin.getRewardManager().addMoneyToPlayerBalance(player, reward.getMoney() * amount);
+							if (inventory.getType() != InventoryType.ANVIL
+									&& inventory.getType() != InventoryType.ENCHANTING
+									&& inventory.getType() != InventoryType.CRAFTING) {
+								if (clickedInventory.getType() == InventoryType.PLAYER) {
+									plugin.getMessages().debug("%s moved %s %s out of the Player Inventory",
+											player.getName(), reward.getMoney(), reward.getDisplayName());
+									plugin.getRewardManager().removeMoneyFromPlayerBalance(player, reward.getMoney());
+								} else { // CHEST, DISPENSER, DROPPER, ......
+									plugin.getMessages().debug("%s moved %s %s into the Player Inventory",
+											player.getName(), reward.getMoney(), reward.getDisplayName());
+									plugin.getRewardManager().addMoneyToPlayerBalance(player, reward.getMoney());
+								}
+							} else {
+								plugin.getMessages().debug("%s: this reward can't be moved into %s", player.getName(),
+										inventory.getType());
+								event.setCancelled(true);
+								return;
 							}
-							// } else {
-							// plugin.getMessages().debug("The slotType is %s",slotType);
-							// }
-						} else {
-							plugin.getMessages().debug("This was not money");
 						}
-					} else {
-						plugin.getMessages().debug("%s: this reward can't be moved into %s", player.getName(),
-								inventory.getType());
-						event.setCancelled(true);
-						return;
 					}
 					break;
 				case NOTHING:
@@ -1088,7 +1081,6 @@ public class BagOfGoldItems implements Listener {
 								isCursor.setAmount(0);
 								event.setCursor(isCursor);
 							}
-
 						}
 					}
 					break;
@@ -1100,7 +1092,11 @@ public class BagOfGoldItems implements Listener {
 						Reward reward2 = new Reward(imCursor.getLore());
 						int amount_reward1 = isCurrentSlot.getAmount();
 						int amount_reward2 = isCursor.getAmount();
-						if ((reward1.isMoney()) && reward1.getRewardType().equals(reward2.getRewardType())) {
+						if (reward2.isMoney() && slotType == SlotType.ARMOR) {
+							plugin.getMessages().debug("%s tried to moved money in to the helmetslot",
+									player.getName());
+							event.setCancelled(true);
+						} else if ((reward1.isMoney()) && reward1.getRewardType().equals(reward2.getRewardType())) {
 							event.setCancelled(true);
 							if (reward1.getMoney() * amount_reward1
 									+ reward2.getMoney() * amount_reward2 <= Core.getConfigManager().limitPerBag) {
